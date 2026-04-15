fef7aab3b3c9d9b3722116076b8a1bd6b926e913
Thainá <thainapaiva0808@gmail.com>
2026-04-14 21:44:49 -0300

    Update exercicio1.sql

--- 08-aula08/08-2026-2026-04-14_Postgres-aula/ADS-DS2P40/THAINA_RODRIGUES_PAIVA-F363IC2/exercicio-01/exercicio1.sql
+++ 08-aula08/08-2026-2026-04-14_Postgres-aula/ADS-DS2P40/THAINA_RODRIGUES_PAIVA-F363IC2/exercicio-01/exercicio1.sql
@@ -1,1 +1,8 @@
-//exercicio1
+SELECT cnpj, razao_social, nome, data_atividade, estado
+INTO
+	thaina_rodrigues_paiva_f363ic2.relatorio1
+FROM 
+	public.cnpj_brasil
+WHERE
+	estado = 'AP' AND
+	cast (data_atividade AS DATE) > '2024-01-01' limit 2000;
